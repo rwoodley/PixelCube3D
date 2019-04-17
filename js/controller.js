251ede63355e17a44c7f268c13f0f604d6e9a5be
Robert Woodley <blwoodley@yahoo.com>
2019-04-16 23:59:25 -0500

    WIP: almost there!

--- js/controller.js
+++ js/controller.js
@@ -5,28 +5,57 @@ function Controller(numFrames, divId) {
     that.numFrames = numFrames;
     that.divId = divId;
     that.cubeGeometry = new THREE.BoxGeometry(numFrames,numFrames,numFrames);
-    that.animate = function(mesh) {
+//    that.cubeGeometry.applyMatrix(new THREE.Matrix4().makeTranslation(
+//        numFrames/2, numFrames/2, numFrames/2
+//    ))
+
+
+    that.animate = function(cubeMesh, planeMesh) {
         that.frame++;
         if (that.frame%400 == 0) {
             that.phase++;
             that.frame = 0;
-            that.resetSize(mesh);
+            that.resetCubeSize(cubeMesh);
             if (that.phase%3==0) {
                 that.phase = 0;
             }
         }
-        else
-            that.adjustSize(mesh);
+        else {
+            that.adjustCubeSize(cubeMesh);
+            that.adjustPlanePosition(planeMesh);
+        }
         document.getElementById(that.divId).innerText = that.phase + ", " + that.frame;
     }
-    that.resetSize = function(mesh) {
+    that.resetCubeSize = function(mesh) {
         mesh.scale.set( 1.0, 1.0, 1.0);
     }
-    that.adjustSize = function(mesh) {
+    that.adjustCubeSize = function(mesh) {
         var scaleFactor = (that.numFrames - that.frame)/that.numFrames;
-        scaleFactorX = that.phase == 0 ?  scaleFactor : 1.0;
+        var offset = ( that.frame)/2.0;
+        scaleFactorX = that.phase == 2 ?  scaleFactor : 1.0;
         scaleFactorY = that.phase == 1 ?  scaleFactor : 1.0;
-        scaleFactorZ = that.phase == 2 ?  scaleFactor : 1.0;
+        scaleFactorZ = that.phase == 0 ?  scaleFactor : 1.0;
         mesh.scale.set( scaleFactorX, scaleFactorY, scaleFactorZ );
+        mesh.position.set(
+        that.phase == 2 ?  -offset : 1.0,
+        that.phase == 1 ?  -offset : 1.0,
+        that.phase == 0 ?  -offset : 1.0
+        );
+
+//        mesh.scale.set(0.5,1.0,1.0);
+    }
+    that.adjustPlanePosition = function(mesh) {
+        if (that.phase == 0) {
+            mesh.rotation.set(0, 0, 0);
+            mesh.position.set(0,0,301-that.frame);
+        }
+        else if (that.phase == 1) {
+            mesh.rotation.set(Math.PI/2, 0, 0);
+            mesh.position.set(0,301-that.frame,0);
+        }
+        else {
+            mesh.rotation.set(0, Math.PI / 2, 0);
+            mesh.position.set(301-that.frame,0,0);
+        }
     }
 }
